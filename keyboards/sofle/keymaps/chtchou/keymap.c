771d676e3fd16f07f07b1cec5380138ef45e33ba
Christian <tchou.christian@gmail.com>
2021-07-15 00:29:32 +0800

    Correct KC_MINS mispelling

--- keyboards/sofle/keymaps/chtchou/keymap.c
+++ keyboards/sofle/keymaps/chtchou/keymap.c
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_BSLS,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC,  \
   KC_ESC,   KC_Q,   KC_Y,    KC_O,    KC_U,    KC_K,                      KC_J,    KC_D,    KC_N,    KC_M,    KC_SLSH, KC_RBRC,    \
   KC_TAB,   KC_W,   KC_H,    KC_E,    KC_A,    KC_COMM,                   KC_G,    KC_S,    KC_R,    KC_T,    KC_P,    KC_GRV, \
-  KC_LSFT,  KC_Z,   KC_X,    KC_MIN,  KC_QUOT, KC_DOT, KC_MUTE, KC_QWERTY,KC_B,    KC_C,    KC_L,    KC_F,    KC_V,    KC_RSFT, \
+  KC_LSFT,  KC_Z,   KC_X,    KC_MINS, KC_QUOT, KC_DOT, KC_MUTE, KC_QWERTY,KC_B,    KC_C,    KC_L,    KC_F,    KC_V,    KC_RSFT, \
                  KC_LGUI,KC_LALT,KC_LCTRL,KC_I, KC_SPC,            KC_ENT,  KC_BSPC, KC_RCTRL, KC_RALT, KC_RGUI \
 ),
 /* LOWER
